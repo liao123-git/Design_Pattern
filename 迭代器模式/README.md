4b52289d434f4ad916a86899c4fcc06b0ef40d40
liao123-git <56588665+liao123-git@users.noreply.github.com>
2021-01-04 09:12:55 +0800

    Unit11_3

--- 迭代器模式/README.md
+++ 迭代器模式/README.md
@@ -4,17 +4,19 @@
 - 使用者无需知道集合的内部结构（封装）
 
 ### 场景
-- 网页事件绑定
-- Promise
-- jQuery callbacks
-- nodejs 自定义事件
-- nodejs 中：处理 http 请求；多进程通讯
-- vue 和 React 组件生命周期触发
-- vue watch
+- jQuery each
+- ES6 Iterator
+    - ES6 语法中，有序集合的数据类型已经有很多
+    - Array Map Set String TypedArray arguments NodeList
+    - 需要有一个统一的遍历接口来遍历所有数据类型
+    - 以上数据类型，都有 [Symbol.iterator] 属性
+    - 属性值是函数，执行函数返回一个迭代器
+    - 这个迭代器就有 next 方法可顺序迭代子元素
+    - for...of 就是 iterator
 
 ### 设计原则验证
 - 迭代器对象和目标对象分离
 - 迭代器将使用者与目标对象隔离开
 - 符合开放封闭原则
 
-### [样例](https://github.com/liao123-git/Design_Pattern/blob/main/%E8%A7%82%E5%AF%9F%E8%80%85%E6%A8%A1%E5%BC%8F/src/index.js "样例")
+### [样例](https://github.com/liao123-git/Design_Pattern/blob/main/%E8%BF%AD%E4%BB%A3%E5%99%A8%E6%A8%A1%E5%BC%8F/src/index.js "样例")
--- 迭代器模式/src/index.js
+++ 迭代器模式/src/index.js
@@ -1,3 +1,6 @@
+/*
+//jQuery each
+
 let arr = [1, 2, 3];
 let nodeList = document.getElementsByTagName('p');
 let $p = $('p');
@@ -26,3 +29,57 @@ function each(data) {
 each(arr);
 each(nodeList);
 each($p);
+*/
+
+class Iterator {
+    constructor(container) {
+        this.list = container.list;
+        this.index = 0;
+    }
+
+    next() {
+        if (this.hasNext()) {
+            return this.list[this.index++];
+        }
+    }
+
+    hasNext() {
+        return this.index < this.list.length;
+    }
+}
+
+class Container {
+    constructor(list) {
+        this.list = list;
+    }
+
+    getIterator() {
+        return new Iterator(this);
+    }
+}
+
+let container = new Container([1, 2, 3, 4, 5, 6]);
+let iterator = container.getIterator();
+while (iterator.hasNext()) {
+    console.log(iterator.next());
+}
+
+function each(data) {
+    let iterator = data[Symbol.iterator]();
+
+    let item = {done: false};
+    while(!item.done){
+        item = iterator.next();
+        !item.done && console.log(item.value);
+    }
+
+    for (let item of data){
+        console.log(item)
+    }
+}
+
+each([1,2,3,4,5]);
+let map = new Map();
+map.set('a',123);
+map.set('b',321);
+each(map);
