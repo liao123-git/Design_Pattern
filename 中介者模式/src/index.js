2b73544a0d76a64d4c1779a0c3610cdcaf768ff6
liao123-git <56588665+liao123-git@users.noreply.github.com>
2021-01-04 14:42:12 +0800

    Unit22

---
+++ 中介者模式/src/index.js
@@ -0,0 +1,48 @@
+class A {
+    constructor() {
+        this.number = 0;
+    }
+
+    setNumber(num, m) {
+        this.number = num;
+        m && m.setB();
+    }
+}
+
+
+class B {
+    constructor() {
+        this.number = 0;
+    }
+
+    setNumber(num, m) {
+        this.number = num;
+        m && m.setA();
+    }
+}
+
+// 中介者
+class Mediator {
+    constructor(a, b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    setB() {
+        let number = this.a.number;
+        this.b.setNumber(number * 100);
+    }
+
+    setA() {
+        let number = this.b.number;
+        this.a.setNumber(number / 100);
+    }
+}
+
+let a = new A();
+let b = new B();
+let m = new Mediator(a, b);
+a.setNumber(100, m);
+console.log(a.number, b.number);
+b.setNumber(100, m);
+console.log(a.number, b.number)
